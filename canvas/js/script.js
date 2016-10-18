8924ea4ee6e00358b77a7256b3d00123550b0a26
major <echolabstech@gmail.com>
2016-10-18 00:06:12 -0400

    add logo / icon to wheel slices

--- canvas/js/script.js
+++ canvas/js/script.js
@@ -1,28 +1,55 @@
 var canvas = document.getElementById('canvas');
 if (canvas.getContext) {
     var ctx = canvas.getContext('2d');
-    let colors = ["orange", "red", "blue"];
+    let colors = ["orange", "red", "blue", "brown", "yellow", "gray"];
     let x = 75;
     let y = 75;
+    let radius = 75;
+    let radians = (Math.PI / 180);
+    let startangle = radians * 0;
+    let endangle = radians * 360;
+    let anticlockwise = false;
+    let icon = 'img/phone-charger.png';
 
-    var startangle, endangle = 0;
-
-    drawArc(ctx);
+    drawArc(ctx, x, y, radius, startangle, endangle, anticlockwise);
+    ctx.save();
     ctx.translate(x, y);
 
     for (let index = 0; index < colors.length; index++) {
-        startangle = (Math.PI / 180) + index;
-        endangle = (Math.PI / 180) + index + 1;
-        drawPieSlice(ctx, index, colors[index], startangle, endangle);
-        drawText(ctx, index);
+        startangle = radians + index;
+        endangle = radians + index + 1;
+        drawPieSlice(ctx, radius, startangle, endangle, anticlockwise, colors[index]);
+        drawText(ctx, (radius / 5), -5, endangle);
+        drawIcon(ctx, icon,(radius / 2), (radius / 2), 15, 15, startangle);
     }
+
+    ctx.restore();
 }
 
-function drawPieSlice(ctx,offset,color, startangle, endangle) {
-    let radius = 50;
-    console.log('startangle:'+startangle+' - endangle:'+endangle)
-    let anticlockwise = false;
-    
+function drawIcon(ctx,src,dx,dy,dh,dw,rotation) {
+    let img = new Image();
+    img.addEventListener("load", function(event) {  
+        ctx.save();
+
+        //position icon
+        ctx.translate(75, 75);
+        ctx.rotate(rotation);
+
+        ctx.save();
+
+        //lineup with text
+        ctx.translate(dx, dy);
+        ctx.rotate((Math.PI / 180) * 35);
+        ctx.drawImage(img, 7, -25, dh, dw);
+
+        ctx.restore();
+
+        ctx.restore();
+    }, false);
+    img.src = src;
+}
+
+function drawPieSlice(ctx,radius,startangle,endangle,anticlockwise,color) {    
     ctx.save();
     ctx.fillStyle = color;
     ctx.beginPath();
@@ -33,46 +60,33 @@ function drawPieSlice(ctx,offset,color, startangle, endangle) {
     ctx.restore();
 }
 
-function drawText(ctx,index) {
-    let rotation = (Math.PI / 180) + index + 1;
+function drawText(ctx,x,y,rotation) {
+    ctx.fillStyle = "black";
+    ctx.font = 'normal Arial';
+    ctx.textAlign = 'left';
+    ctx.textBaseline = 'Alphabetic';
+    let radians = (Math.PI / 180);
 
     ctx.save();
-    ctx.fillStyle = "black";
+
     ctx.rotate(rotation);
-    ctx.font = 'normal Arial';
-    ctx.fillText("Hello", 0, 0);
+
+    ctx.save();
+
+    ctx.translate(x, y);
+    ctx.rotate(radians * -25);
+    ctx.fillText("Hello", 2.5, 0);
+
+    ctx.restore();
+    
     ctx.restore();
 }
 
-function drawArc(ctx) {
-    let x = 75;
-    let y = 75;
-    let radius = 50;
-    let startangle = (Math.PI / 180) * 0;
-    let endangle = (Math.PI / 180) * 360;
-    let anticlockwise = false;
-    
-    ctx.fillStyle = "black";
+function drawArc(ctx,x,y,radius,startangle,endangle,anticlockwise,fillstyle) {
+    ctx.fillStyle = fillstyle;
     ctx.beginPath();
     ctx.arc(x, y, radius, startangle, endangle, anticlockwise);
     ctx.stroke();
-}
-
-function drawRandomThing(ctx) {
-    ctx.save();
-    ctx.fillStyle = '#B5121B';
-    ctx.translate(50, 25);
-    ctx.rotate((Math.PI / 180) * 90);
-    ctx.beginPath();
-    ctx.arc(5, 5, 100, 5.5, 6);
-    ctx.lineTo(0, 0);
-    ctx.closePath();
-    ctx.fill();
-    ctx.save();
-    ctx.fillStyle = '#000000';
-    ctx.save();
-    ctx.fillText("Hello world", 20, 20);
-    ctx.restore();
 }
 
 function drawSmiley(ctx) {
