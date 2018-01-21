1c355794654eeb1736db5149db18a6831087a907
echolabstech <echolabstech@gmail.com>
2018-01-20 23:01:20 -0500

    npm install webpack dev

---
+++ reactjs/workflows/development/webpack.config.js
@@ -0,0 +1,15 @@
+var webpack = require("webpack");
+var path = require("path");
+
+var DEV = path.resolve(__dirname, "dev/scripts");
+var OUTPUT = path.resolve(__dirname, "static");
+
+var config = {
+  entry: DEV + "/app.jsx",
+  output: {
+    path: OUTPUT,
+    filename: "script.js"
+  }
+};
+
+module.exports = config;
