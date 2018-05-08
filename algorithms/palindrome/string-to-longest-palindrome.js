9468e6ce268c6ac6ad11ca4ef197fc2694a04a59
echolabstech <echolabstech@gmail.com>
2018-05-07 21:22:05 -0400

    palindrome algorithms

---
+++ algorithms/palindrome/string-to-longest-palindrome.js
@@ -0,0 +1,11 @@
+// first identify a palindrome
+const samplePalindrome = "mom"
+
+// identify not a palindrome
+const notPalindrome = "tom"
+
+// find the palindrome in a simple string
+const containsPalindrome = "i love mom"
+
+// find the longest palindrome
+const palindromeString = "i love mom and dad, but not the racecar"
