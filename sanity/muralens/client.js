ae561c46f42f2bbf4550dfd1d376852a0549a933
echolabstech <echolabstech@gmail.com>
2018-05-22 23:47:45 -0400

    movie client code to module and bundle with webpack

--- sanity/muralens/client.js
+++ sanity/muralens/client.js
@@ -1,5 +1,7 @@
 const sanityClient = require('@sanity/client')
 const client = sanityClient({
   projectId: 'mjtpwlkb',
-  dataset: 'development'
+  dataset: 'development',
+  useCdn: true,
+  token: 'skpSgBjz0MwmOn1yXHjnKnOTZji5wLio1jmXYhB4AbQJ0MR2ZVWiAU2ypKtbuCS8hwQsb4UIBxfZH4MKXxmqHP75ZTtNMlSynczsIYpfKkJezCwANBL5cLSIDj2eOqGdXavyNR2c5i52fY83DNv5mspfjEE3o8XNsHX0G2tQAHgVos5fnahj'
 });
--- sanity/muralens/webpack.config.js
+++ sanity/muralens/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
 	watch: true,
 	mode: 'development',
-	entry: './fetch-neighborhoods.js',
+	entry: ['./client.js', './fetch-neighborhoods.js'],
 	output: {
 		filename: './bundle.js',
 		path: path.resolve(__dirname)
