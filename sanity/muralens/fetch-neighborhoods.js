15d17d87836f9d4f21a1e737e891c8d7bd2d11e4
echolabstech <echolabstech@gmail.com>
2018-05-23 00:08:51 -0400

    cleanup fetch-neighborhood.js

--- sanity/muralens/fetch-neighborhoods.js
+++ sanity/muralens/fetch-neighborhoods.js
@@ -1,12 +1,3 @@
-const FuzzySearch = require('fuzzy-search');
-const sanityClient = require('@sanity/client')
-const client = sanityClient({
-  projectId: 'mjtpwlkb',
-  dataset: 'development',
-  useCdn: true,
-  token: 'skpSgBjz0MwmOn1yXHjnKnOTZji5wLio1jmXYhB4AbQJ0MR2ZVWiAU2ypKtbuCS8hwQsb4UIBxfZH4MKXxmqHP75ZTtNMlSynczsIYpfKkJezCwANBL5cLSIDj2eOqGdXavyNR2c5i52fY83DNv5mspfjEE3o8XNsHX0G2tQAHgVos5fnahj'
-});
-
 function fetchFromAPI() {
 	client.fetch(
 	  '*[_type == $type]', // Query
@@ -27,18 +18,8 @@ function fetchFromLocal() {
 	})
 	.then(locations => {
 		console.log('all neighborhoods: ', locations);
-		fuzzySearch('detroit', locations);
 	})
 	.catch(error => {
 		console.error('Oh no, error occured: ', error);
 	});
 }
-
-function fuzzySearch(needle, haystack) {
-	const searcher = new FuzzySearch(haystack, ['name'], {caseSensitive: false});
-	const result = searcher.search(needle);
-	console.log(result);
-}
-
-fetchFromLocal();
-
