06a870e86577e5c7828da3c7b30f92c2a2653a38
echolabstech <echolabstech@gmail.com>
2018-05-25 00:52:53 -0400

    use geolocation to get current location

--- sanity/muralens/app.js
+++ sanity/muralens/app.js
@@ -1,7 +1,11 @@
 import {log, error} from './logger.js';
 
 if ('geolocation' in navigator) {
-	log('geolocation exists navigator');
+	navigator.geolocation.getCurrentPosition(function(position) {
+		log(position.coords.latitude, position.coords.longitude);
+	}, function(error) {
+		error('geolocation.getCurrentPosition() not working');
+	});
 } else {
 	error('geolocation API not available');
 }
