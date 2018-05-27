3f196ba5a377d27a08d50a4e25486b5eaab218a3
echolabstech <echolabstech@gmail.com>
2018-05-27 13:50:18 -0400

    ant-eater

---
+++ playground/ant-eater.js
@@ -0,0 +1,11 @@
+function runSimulation(initialStateOfWorld) {
+	const lmr = '>'; // leftMovingRight
+	const rml = '<'; // rightMovingLeft
+	const cross = '+'; // antsCrossingPaths
+	const empty = '.';
+}
+
+const ants = ['>', '>', '.', '.', '.', '<', '<'];
+console.log(ants);
+const world = runSimulation();
+console.log(world);
