11a40541fd08c6e95e6563cc09b113187d73fca3
echolabstech <echolabstech@gmail.com>
2018-05-29 21:08:11 -0400

    setup orm db connection and import into app.js

--- products-database/products-database/app.js
+++ products-database/products-database/app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var sequelize = require('./db/orm.js');
+
 var app = express();
 
 // view engine setup
