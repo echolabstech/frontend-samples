6d774844fc40a015d6bcd25758fe284a76c22764
major <echolabstech@gmail.com>
2020-04-30 18:54:37 +0800

    filter by median per-capita (per person) income

--- random-json/country_filter.py
+++ random-json/country_filter.py
@@ -24,6 +24,11 @@ def get_countries():
 			countries[country] = countries_by_climate[country]
 	return countries
 
+def get_countries_by_median_income():
+	with open('median-income-by-country.json') as file:
+		countries_by_median_income = json.load(file)
+	return countries_by_median_income
+
 def filter_countries_by_climate(countries, climates):
 	matching_countries = {}
 	for country in countries:
@@ -31,8 +36,22 @@ def filter_countries_by_climate(countries, climates):
 			matching_countries[country] = countries[country]
 	return matching_countries
 
+def filter_countries_by_median_income(countries):
+	exchange_rate_rmb_to_usd = 7.06
+	monthly_expenses_usd = 15000 / exchange_rate_rmb_to_usd
+	annual_expenses_usd = monthly_expenses_usd * 12
+	countries_by_median_income = get_countries_by_median_income()
+	affordable_countries = {}
+	for country in countries_by_median_income['data']:
+		if country['medianHouseholdIncome'] <= annual_expenses_usd:
+			if country['name'] in countries:
+				affordable_countries[country['name']] = countries[country['name']]
+	return affordable_countries
+
 countries = get_countries()
 climates = ['tropical', 'mediterranean', 'hot', 'humid']
 tropical_countries = filter_countries_by_climate(countries, climates)
-pprint(tropical_countries)
-pprint(len(tropical_countries))
+
+affordable_countries = filter_countries_by_median_income(tropical_countries)
+pprint(affordable_countries)
+pprint(len(affordable_countries))
