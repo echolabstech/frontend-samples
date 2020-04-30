de838a267faaff6b97695dab276559d3b2e8c9dd
major <echolabstech@gmail.com>
2020-04-30 17:04:33 +0800

    extract countries from visa free and climate from json files

--- random-json/country_filter.py
+++ random-json/country_filter.py
@@ -1,12 +1,18 @@
 import json
-
-with open('countries-by-climate.json') as file:
-	countries_by_climate = json.load(file)
-
-with open('visa-free-countries-for-chinese.json') as file:
-	no_visa_countries = json.load(file) # countries chinese citizens can enter w/o visa
-
 from pprint import pprint
-for continent in no_visa_countries:
-	for country in continent['flags']:
-		pprint(country['name'])
+
+def get_country_by_climate():
+	with open('countries-by-climate.json') as file:
+		countries_by_climate = json.load(file)
+	return countries_by_climate
+
+def get_country_by_visa_free():
+	with open('visa-free-countries-for-chinese.json') as file:
+		no_visa_countries = json.load(file) # countries chinese citizens can enter w/o visa
+	country_names = []
+	for continent in no_visa_countries:
+		for country in continent['flags']:
+			country_names.append(country['name'])
+	return country_names
+
+pprint(get_country_by_climate())
