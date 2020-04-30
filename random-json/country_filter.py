d2815632d31b2447e1835c6cfba71b31feff0284
major <echolabstech@gmail.com>
2020-04-30 15:22:00 +0800

    load json

--- random-json/country_filter.py
+++ random-json/country_filter.py
@@ -1,4 +1,9 @@
 import json
 
-countriesByClimate = {}
-noVisaCountries = {}# countries chinese citizens can enter w/o visa
+with open('countries-by-climate.json') as file:
+	countriesByClimate = json.load(file)
+
+with open('visa-free-countries-for-chinese.json') as file:
+	noVisaCountries = json.load(file) # countries chinese citizens can enter w/o visa
+
+print(noVisaCountries)
