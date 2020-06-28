4b07a40aeeb36c2dbf5a238b0cb2902d278b267a
major <echolabstech@gmail.com>
2020-06-28 23:45:36 +0800

    WIP test

--- react-practice/createReactApp/tic-tac-toe/src/Game.test.js
+++ react-practice/createReactApp/tic-tac-toe/src/Game.test.js
@@ -2,8 +2,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Game from './Game';
+import {render} from '@testing-library/react'
+import {getByText} from '@testing-library/dom'
+import '@testing-library/jest-dom/extend-expect'
 
-it('renders without crashing', () => {
+it('Game renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Game />, div);
+});
+
+test('WIP', () => {
+  // render(<Game />);
+  // expect(getByText('Hello, world!')).toBeInTheDocument();
 });
