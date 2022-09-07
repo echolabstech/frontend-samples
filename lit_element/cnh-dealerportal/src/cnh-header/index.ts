e9a52b375dcd519626d5d13f83cc9576b4d7e595
major <echolabstech@gmail.com>
2022-09-07 09:22:55 -0400

    add logo to header

--- lit_element/cnh-dealerportal/src/cnh-header/index.ts
+++ lit_element/cnh-dealerportal/src/cnh-header/index.ts
@@ -1,10 +1,29 @@
-import { LitElement, html, } from 'lit';
+import { LitElement, html, css, } from 'lit';
+
+const logo = new URL('/assets/open-wc-logo.svg', import.meta.url).href;
 
 export default class CnhHeader extends LitElement {
+	static styles = css`
+		header {
+			display: flex;
+			background-color: #fff;
+		}
+
+		.logo {
+			width: 10%;
+		}
+
+		.logo img {
+			width: 100%;
+		}
+	`;
+
 	render() {
 		return html`
-			<header>this is the header</header>
-		`
+			<header>
+				<span class="logo"><img alt="open-wc logo" src=${logo} /></span>
+			</header>
+		`;
 	}
 }
 customElements.define('cnh-header', CnhHeader);
